70919c41b6216df40aea967170501b9703bde817
Никита Сидоренко <nikitastar280@gmail.com>
2021-12-04 20:57:33 +0300

    update

--- app/src/main/java/com/example/tutors/Activity/TutorPageActivity.java
+++ app/src/main/java/com/example/tutors/Activity/TutorPageActivity.java
@@ -1,19 +1,25 @@
 package com.example.tutors.Activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.tutors.Models.ItemsTypes;
 import com.example.tutors.Models.Tutor;
 import com.example.tutors.R;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TutorPageActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,9 +42,24 @@ public class TutorPageActivity extends AppCompatActivity {
             lastName.setText(tutor.getLastName());
             phoneNumber.setText(tutor.getPhoneNumber());
             rating.setText(tutor.getStringRating());
-            items.setText(tutor.getStringItems());
+            items.setText(getStringItems(tutor));
             description.setText(tutor.getDescription());
             // avatar.setImageURI(URI.parse("file://mnt/sdcard/cat.jpg"));
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String getStringItems(Tutor tutor)
+    {
+        String[] items_array_string = getResources().getStringArray(R.array.items_array_rus);
+        List<ItemsTypes> itemsTypes = tutor.getItems();
+
+        List<String> resArray = new ArrayList<>();
+        for(ItemsTypes item: itemsTypes)
+        {
+            resArray.add(items_array_string[item.ordinal()]);
+        }
+
+        return String.join(", ", resArray);
     }
 }
